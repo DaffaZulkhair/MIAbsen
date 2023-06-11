31e91ab298e95904a2c2e1f19061f47a340f5901
byy7 <muhammad.faraby710@gmail.com>
2023-06-11 15:40:48 +0700

    update

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -140,6 +140,7 @@
         </div>
     </div>
 @endsection
+
 @section('js_after')
     {{-- Select 2 --}}
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -149,3 +150,4 @@
             $(".select_user").select2();
         });
     </script>
+@endsection
